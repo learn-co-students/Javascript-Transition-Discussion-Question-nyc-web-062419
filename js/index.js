d3158ca4515a78730705567ca044710838cca440
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-08 13:14:45 -0400

    dq3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+let button = document.querySelector('#save_lincoln')
+let input = document.querySelector('#interval')
+
+function fadeOutEffect(num) {
+  var fadeTarget = document.getElementById("foreground");
+  var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+        console.log("part 1")
+          fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+        console.log("part 2")
+          fadeTarget.style.opacity -= .1;
+      } else {
+        console.log("part 3")
+        clearInterval(fadeEffect);
+      }
+      console.log("part 4")
+  }, num);
 }
+button.addEventListener('click', function(e) {
+  if (input.value !== "") {
+    fadeOutEffect(parseInt(input.value)/10)
+  }
+  
+});
